74be0b176554ed29af7ab83354654ed632813fa6
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-19 22:57:08 +0300

    Update POSController.php

--- src/Http/Controllers/POSController.php
+++ src/Http/Controllers/POSController.php
@@ -1,1 +1,30 @@
+namespace Webkul\POSPayment\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Config;
+
+class POSController extends Controller
+{
+    public function processPayment(Request $request, $bank)
+    {
+        $bankConfig = Config::get("pospayment.banks.$bank");
+
+        if (!$bankConfig) {
+            return response()->json(['error' => 'Desteklenmeyen banka'], 400);
+        }
+
+        // Ödeme işlemi API'ye gönder
+        $response = $this->sendPaymentToBank($bankConfig, $request->all());
+
+        return response()->json($response);
+    }
+
+    private function sendPaymentToBank($config, $data)
+    {
+        // Banka API'sine ödeme gönderme işlemi
+        return [
+            'status' => 'success',
+            'message' => 'Ödeme başarıyla gerçekleştirildi!'
+        ];
+    }
+}
