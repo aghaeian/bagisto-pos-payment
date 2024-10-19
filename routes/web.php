68cd994c6bc5f10423265d696e5ef1c9787a9229
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-19 22:58:52 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,5 @@
+use Webkul\POSPayment\Http\Controllers\POSController;
 
+Route::prefix('pos-payment')->group(function () {
+    Route::post('/process/{bank}', [POSController::class, 'processPayment'])->name('pospayment.process');
+});
