2419ba6f81c37472ec37088e0dc5a068a49ad3f7
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-20 00:09:35 +0300

    Update POSPayment.php

--- src/Payment/POSPayment.php
+++ src/Payment/POSPayment.php
@@ -1,1 +1,20 @@
+<?php
 
+namespace Aghaeian\POSPayment\Payment;
+
+use Webkul\Payment\Payment\Payment;
+
+class POSPayment extends Payment
+{
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $code = 'pospayment';
+
+    public function getRedirectUrl()
+    {
+        return route('pospayment.process');
+    }
+}
