877f0dec0f46a157ef4181b25a8bf31723d68cce
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-19 22:58:24 +0300

    Update InstallPOSPayment.php

--- src/Console/Commands/InstallPOSPayment.php
+++ src/Console/Commands/InstallPOSPayment.php
@@ -1,1 +1,29 @@
+namespace Webkul\POSPayment\Console\Commands;
 
+use Illuminate\Console\Command;
+use Brotzka\DotenvEditor\DotenvEditor;
+
+class InstallPOSPayment extends Command
+{
+    protected $signature = 'pospayment:install';
+    protected $description = 'POS ödeme paketi için gerekli ayarları yapılandırma';
+
+    public function handle()
+    {
+        $this->info('POS Ödeme Yöntemi kuruluyor...');
+
+        $env = new DotenvEditor;
+
+        // Kullanıcıdan merchant ID ve secret key'leri soruyoruz
+        $akbankMerchantId = $this->ask('Akbank Merchant ID');
+        $akbankSecretKey = $this->ask('Akbank Secret Key');
+        
+        $env->setKey('AKBANK_MERCHANT_ID', $akbankMerchantId);
+        $env->setKey('AKBANK_SECRET_KEY', $akbankSecretKey);
+
+        // Diğer bankalar için sorular eklenebilir
+        $env->save();
+
+        $this->info('POS Ödeme Yöntemi başarıyla kuruldu.');
+    }
+}
