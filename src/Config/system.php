c8c568c96c37a1e11990a2944ba6b85fc49cd21d
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-20 00:07:51 +0300

    Update system.php

--- src/Config/system.php
+++ src/Config/system.php
@@ -1,1 +1,18 @@
 
+<?php
+
+return [
+    'payment' => [
+        'methods' => [
+            'pospayment' => [
+                'code' => 'pospayment',
+                'title' => 'POS Payment Method',
+                'description' => 'Supports multiple Turkish banks',
+                'class' => 'Aghaeian\POSPayment\Payment\POSPayment',
+                'active' => true, // Make sure this is true
+                'sort' => 1, // This controls the display order of the payment methods
+                'default' => false,
+            ],
+        ],
+    ],
+];
