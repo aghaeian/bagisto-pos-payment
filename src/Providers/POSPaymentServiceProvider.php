96953b011a796d4be7e15257a122fe49cf07ecaa
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-19 22:57:43 +0300

    Update POSPaymentServiceProvider.php

--- src/Providers/POSPaymentServiceProvider.php
+++ src/Providers/POSPaymentServiceProvider.php
@@ -1,1 +1,24 @@
+namespace Webkul\POSPayment\Providers;
 
+use Illuminate\Support\ServiceProvider;
+use Webkul\POSPayment\Console\Commands\InstallPOSPayment;
+
+class POSPaymentServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__ . '/../Config/config.php', 'pospayment'
+        );
+
+        $this->commands([
+            InstallPOSPayment::class,
+        ]);
+    }
+
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../../Resources/views', 'pospayment');
+    }
+}
