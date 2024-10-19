96d68e3a15c5e6c8b97bbb4fb50ba08dc2e557cd
Hamed <115875025+aghaeian@users.noreply.github.com>
2024-10-19 23:58:46 +0300

    Update InstallPOSPayment.php

--- src/Console/Commands/InstallPOSPayment.php
+++ src/Console/Commands/InstallPOSPayment.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace Aghaeian\POSPayment\Console\Commands;
 
 use Illuminate\Console\Command;
@@ -5,23 +7,39 @@ use Brotzka\DotenvEditor\DotenvEditor;
 
 class InstallPOSPayment extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'pospayment:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
     protected $description = 'POS ödeme paketi için gerekli ayarları yapılandırma';
 
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
     public function handle()
     {
         $this->info('POS Ödeme Yöntemi kuruluyor...');
 
         $env = new DotenvEditor;
 
-        // Kullanıcıdan merchant ID ve secret key'leri soruyoruz
+        // Ask for the merchant IDs and secret keys for each bank
         $akbankMerchantId = $this->ask('Akbank Merchant ID');
         $akbankSecretKey = $this->ask('Akbank Secret Key');
         
         $env->setKey('AKBANK_MERCHANT_ID', $akbankMerchantId);
         $env->setKey('AKBANK_SECRET_KEY', $akbankSecretKey);
 
-        // Diğer bankalar için sorular eklenebilir
+        // You can add more banks here as needed
         $env->save();
 
         $this->info('POS Ödeme Yöntemi başarıyla kuruldu.');
